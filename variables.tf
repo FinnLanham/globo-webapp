0c1f02d59f1fed49dc0384235509d2bbd02f7818
Some Body <finley.lanham@gmail.com>
2026-02-21 15:25:41 +0000

    Switch to using secrets manager

--- variables.tf
+++ variables.tf
@@ -32,9 +32,9 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "api_key" {
+variable "api_key_secret_id" {
   type        = string
-  description = "(Required) API key for web app to talk to SaaS platform."
+  description = "(Required) Secret ID for API key for web app to talk to SaaS platform."
 }
 
 variable "tfe_organization" {
@@ -50,4 +50,9 @@ variable "tfe_workspace_name" {
 variable "playbook_repository" {
   type        = string
   description = "(Required) URL of Ansible Playbook"
+}
+
+variable "ec2_role_name" {
+  type        = string
+  description = "(Required) Role name for EC2 instnace"
 }
